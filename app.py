b66412e2f3851f3322d7bd0e3e8b3dfba0800620
jmagnasciutti <jmagnasciutti@gmail.com>
2026-03-30 11:24:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.chains import RetrievalQA
-
+from langchain_community.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 # 1. CONFIGURACIÓN
 st.set_page_config(page_title="LexScout: IA Legal", page_icon="⚖️")
 
